8d9bac066e2b25e967d1ffd7f027537fdd9853b5
chandraprabhu1602 <chandraprabhu1602@gmail.com>
2020-08-20 20:19:08 +0530

    m

--- myjs01.js
+++ myjs01.js
@@ -1,3 +1,16 @@
 var a;
 a = 10
 console.log(a);
+function fun1(name){
+  document.getElementById("content").innerHTML=("hello "+document.getElementById("input1").value);
+}
+
+//mousemove Calsulator
+/*
+document
+  .querySelector("body")
+  .addEventListener("mousemove",function(){
+    console.log("Hi");
+  }
+);
+*/
